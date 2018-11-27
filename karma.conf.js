9931b4a8dcaf051a828d24a73f9559df255e1f51
ylerjen <lindel13@hotmail.com>
2018-11-27 21:36:06 +0100

    develop add script file

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: '.',
 
 
     // frameworks to use
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'dist/**/*.js',
       'tests/**/*.spec.js'
     ],
 
@@ -55,7 +56,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless', 'IE', 'Firefox'],
+    browsers: [
+      // 'Chrome',
+      // 'ChromeHeadless',
+      'IE',
+      // 'Firefox'
+    ],
 
 
     // Continuous Integration mode
