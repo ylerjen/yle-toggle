9ec8488dead3969e79c3e7655f529f1c200680e3
ylerjen <lindel13@hotmail.com>
2018-11-27 18:52:44 +0100

    develop define a basic webcomponent

--- src/scripts/component.js
+++ src/scripts/component.js
@@ -1,17 +1,68 @@
 import componentDom from './../templates/component.html';
+import componentStyles from './../styles/component.scss';
 
-export class WebComponent extends HTMLElement {
+function createTemplate() {    
+    const tplWrapper = document.createElement('template');    
+    tplWrapper.innerHTML = `<style>${componentStyles}</style>${componentDom}`;
+    return tplWrapper;
+}
+
+/**
+ * TODO jsdoc
+ * 
+ * @class ChangeMe
+ */
+export class ChangeMe extends HTMLElement {
+    /**
+     * @static
+     * 
+     */
+    static get observedAttributes() { return []; }
+
+    /**
+     * Constructor of the class. It's called when an instance of the element is created or upgraded.
+     * Useful for initializing state, settings up event listeners, or creating shadow dom.
+     * See the spec for restrictions on what you can do in the constructor.
+     */
     constructor() {
         super();
-        const tplWrapper = document.createElement('template');
-        tplWrapper.innerHTML = componentDom;
-        const content = tplWrapper.content;
+        const content = createTemplate().content;
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.appendChild(content.cloneNode(true));
         this.innerHTML = '<h1>I\'m your Web-Component.</h1><p>Change me in <code>src/scripts/component.js</code></p>';
     }
+
+    /**
+     * Called every time the element is inserted into the DOM.
+     * Useful for running setup code, such as fetching resources or rendering.
+     * Generally, you should try to delay work until this time.
+     */
+    connectedCallback() {
+
+    }
+    /**
+     * Called every time the element is removed from the DOM.
+     * Useful for running clean up code.
+     */
+    disconnectedCallback() {
+
+    }
+    /**
+     * Called when an observed attribute listed in the "static observedAttributes" has been added, removed, updated, or replaced
+     * @param {string} attrName - the name of the attribute that changed
+     * @param {string} oldVal - the name of the attribute that changed
+     * @param {string} newVal - the name of the attribute that changed
+     * @see observedAttributes
+     */
+    attributeChangedCallback(attrName, oldVal, newVal) {
+
+    }
 }
   
 export function defineCustomElement() {
-    customElements.define('web-component', WebComponent);
+    // define the web component for a standard HTMLElement extension
+    customElements.define('web-component', ChangeMe);
+    
+    // but if you extend a specific element like HTMLButtonElement you'll need the following declaration instead.
+    // customElements.define('change-me', ChangeMe, {extends: 'button'});
 }
