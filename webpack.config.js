32d7f856f5c72c93e810a59278466de6cb01da1f
ylerjen <lindel13@hotmail.com>
2018-11-27 15:27:32 +0100

    develop use babel loader for esnext

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,39 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    watchContentBase: true
-  },
-  plugins: [
-      new CopyWebpackPlugin([
-        { from: 'src/index.html' },
-      ])
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        watchContentBase: true
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/index.html' },
+        ])
     ],
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }, {
                 test: /\.html$/,
                 exclude: /node_modules/,
                 use: {loader: 'html-loader'}
             }
-  ]
+        ]
     }
 };
